be85b4c1671aa39646c3423085e6386f8c520076
alents <alexlents@gmail.com>
2019-11-15 10:52:00 +0500

    first commit

---
+++ src/test/java/lenz/home/cookbook/CookbookApplicationTests.java
@@ -0,0 +1,15 @@
+package lenz.home.cookbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+        String s = "abc\nabc";
+        System.out.println(s);
+    }
+
+}
